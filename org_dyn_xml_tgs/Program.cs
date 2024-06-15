f0286ea4f978980068db63da613c5c73760965ac
ravikoppula <ravikoppula92@gmail.com>
2024-06-15 17:38:06 +0800

    removed root element as default wrapper

--- org_dyn_xml_tgs/Program.cs
+++ org_dyn_xml_tgs/Program.cs
@@ -10,21 +10,34 @@ class Program
         string appHdrInput = "AppHdr|AppHdr/Fr|AppHdr/Fr/FIId|AppHdr/Fr/FIId/FinInstId|AppHdr/Fr/FIId/FinInstId/Othr|AppHdr/Fr/FIId/FinInstId/Othr/Id|AppHdr/To|AppHdr/To/FIId|AppHdr/To/FIId/FinInstId|AppHdr/To/FIId/FinInstId/Othr|AppHdr/To/FIId/FinInstId/Othr/Id|AppHdr/BizMsgId|AppHdr/MsgDefIdr|AppHdr/BizSvc|AppHdr/CreDt|AppHdr/CpyDplct|AppHdr/PssblDplct|AppHdr/Sgntr|AppHdr/Sgntr/Signature";
         string fitoInput = "FIToFICstmrCdtTrf|FIToFICstmrCdtTrf/GrpHdr|FIToFICstmrCdtTrf/GrpHdr/MsgId|FIToFICstmrCdtTrf/GrpHdr/CreDtTm|FIToFICstmrCdtTrf/GrpHdr/NbOfTxns|FIToFICstmrCdtTrf/GrpHdr/SttlmInf|FIToFICstmrCdtTrf/GrpHdr/SttlmInf/SttlmtMtd|FIToFICstmrCdtTrf/CdtTrfTxnInf|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtId|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtId/EndToEndId|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtId/TxnId|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtId/ClrSysRef|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtTpInf|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtTpInf/CtgyPurp|FIToFICstmrCdtTrf/CdtTrfTxnInf/PmtTpInf/CtgyPurp/Prtry|FIToFICstmrCdtTrf/CdtTrfTxnInf/IntrBkSttlmAmt|FIToFICstmrCdtTrf/CdtTrfTxnInf/IntrBkSttlmDt|FIToFICstmrCdtTrf/CdtTrfTxnInf/ChrgBr|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/Othr|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/Othr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/Nm|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/PstlAdr|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/PstlAdr/StrtNm|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/PstlAdr/TwnNm|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/PstlAdr/CtrySubDvsn|FIToFICstmrCdtTrf/CdtTrfTxnInf/InstgAgt/FinInstId/PstlAdr/Ctry|FIToFICstmrCdtTrf/CdtTrfTxnInf/Dbtr|FIToFICstmrCdtTrf/CdtTrfTxnInf/Dbtr/Nm|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct/Id/Othr|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct/Id/Othr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct/Tp|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAcct/Tp/Prtry|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAgt|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAgt/FinInstnId|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAgt/FinInstnId/Othr|FIToFICstmrCdtTrf/CdtTrfTxnInf/DbtrAgt/FinInstnId/Othr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAgt|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAgt/FinInstnId|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAgt/FinInstnId/Othr|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAgt/FinInstnId/Othr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/Cdtr|FIToFICstmrCdtTrf/CdtTrfTxnInf/Cdtr/Nm|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct/Id/Othr|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct/Id/Othr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct/Tp|FIToFICstmrCdtTrf/CdtTrfTxnInf/CdtrAcct/Tp/Prtry|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/PlcAndNm|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/RecptRef|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/PymtDesc|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/ScndValInd|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/IdTp|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForCdtrAgt/SellerId|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForDbtrAcct|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForDbtrAcct/RsdnSts|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForDbtrAcct/PrdTp|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForDbtrAcct/ShariaCmpl|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/InstrForDbtrAcct/Dtls|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/Dbtr|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/Dbtr/IpAddr|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/Dbtr/Id|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/CdtTrfTxnInf|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/CdtTrfTxnInf/URL|FIToFICstmrCdtTrf/CdtTrfTxnInf/SplmtryData/Envlp/PrxyLkUpRef";
 
-        XElement root = new XElement("root");
+        //XElement root = new XElement("root");
 
-        AddElements(root, appHdrInput);
-        AddElements(root, fitoInput);
+        //AddElements(root, appHdrInput);
+        //AddElements(root, fitoInput);
 
         // Example values for some of the nodes
-        SetExampleValues(root);
+        //SetExampleValues(root);
 
-        Console.WriteLine(root);
+        //Console.WriteLine(root);
 
-        int nodeCount = root.Descendants().Count();
-        int childNodeCount = root.Descendants().Sum(e => e.Elements().Count());
+        //int nodeCount = root.Descendants().Count();
+        //int childNodeCount = root.Descendants().Sum(e => e.Elements().Count());
 
-        Console.WriteLine($"Total nodes: {nodeCount}");
-        Console.WriteLine($"Total child nodes: {childNodeCount}");
+        //Console.WriteLine($"Total nodes: {nodeCount}");
+        //Console.WriteLine($"Total child nodes: {childNodeCount}");
+
+
+        XElement appHdrRoot = new XElement("AppHdr");
+        XElement fitoRoot = new XElement("FIToFICstmrCdtTrf");
+
+        AddElements(appHdrRoot, appHdrInput);
+        AddElements(fitoRoot, fitoInput);
+
+        // Combine the two root elements into a string without a wrapping root element
+        string combinedXml = $"{appHdrRoot}{fitoRoot}";
+
+        Console.WriteLine(combinedXml);
+
 
         Console.ReadLine();
 
